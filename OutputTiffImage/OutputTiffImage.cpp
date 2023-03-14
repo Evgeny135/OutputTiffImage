1d19f8dd4fce92935abb4e76784e5828a9362ebd
Evgeny135 <75969458+Evgeny135@users.noreply.github.com>
2023-03-14 21:11:42 +0300

    Добавьте файлы проекта.

---
+++ OutputTiffImage/OutputTiffImage.cpp
@@ -0,0 +1,254 @@
+﻿// Project1.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "OutputTiffImage.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_OUTPUTTIFFIMAGE, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OUTPUTTIFFIMAGE));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_OUTPUTTIFFIMAGE));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_OUTPUTTIFFIMAGE);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+unsigned int getBit(char* buffer, int length = 4) {
+    unsigned int size = 0;
+    //int length = sizeof(*buffer / buffer[0]);
+
+    for (int i = 0; i < length; i++)
+    {
+        size += static_cast<uint32_t>(static_cast<uint8_t>(buffer[i])) << 8 * i;
+    }
+    return size;
+}
+
+
+
+struct IFD {
+    unsigned int tagId;
+    unsigned int typeTag;
+    unsigned int length;
+    unsigned int offset;
+    unsigned int value;
+
+    void toString() {
+        cout << "tagId: " << this->tagId << " typeId: " << this->typeTag << " length: " << this->length << " offset: " << this->offset << " value: " << this->value << endl;
+    }
+};
+
+struct Image {
+    int width;
+    int height;
+    int offset;
+
+};
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Разобрать выбор в меню:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        Image img;
+        char buffer[4];
+        std::ifstream in("C:\\Users\\trish\\Desktop\\test.tif", std::ios::binary);
+        in.seekg(4);
+        in.read(buffer, 4);
+        unsigned int offsetIfd = getBit(buffer);
+        in.seekg(offsetIfd);
+        in.read(buffer, 2);
+        unsigned int numberIfd = getBit(buffer, 2);
+        for (int i = 0; i < numberIfd; i++)
+        {
+            IFD ifd;
+            bool valueOff = false;
+            in.seekg(offsetIfd + 2);
+            in.read(buffer, 2);
+            ifd.tagId = getBit(buffer, 2);
+            in.read(buffer, 2);
+            ifd.typeTag = getBit(buffer, 2);
+            if (getBit(buffer, 2) > 3)
+            {
+                valueOff = true;
+            }
+            in.read(buffer, 4);
+            ifd.length = getBit(buffer);
+            in.read(buffer, 4);
+            ifd.offset = getBit(buffer);
+            if (valueOff)
+            {
+                in.seekg(ifd.offset);
+                in.read(buffer, 4);
+                ifd.value = getBit(buffer, 4);
+            }
+            else
+            {
+                ifd.value = ifd.offset;
+            }
+            switch (ifd.tagId)
+            {
+            case 256:
+                img.width = ifd.value;
+                break;
+
+            case 257:
+                img.height = ifd.value;
+                break;
+            case 273:
+                img.offset = ifd.value;
+            default:
+                break;
+            }
+
+            offsetIfd += 12;
+
+        }
+        char pixels[3];
+        in.seekg(img.offset);
+        for (int y = 0; y < img.height; y++)
+        {
+            for (int x = 0; x < img.width; x++)
+            {
+                in.read(pixels, 3);
+                SetPixel(hdc, x, y, RGB(pixels[0], pixels[1], pixels[2]));
+            }
+        }
+        in.close();
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
